6ace64d714855575bb279ab7d17af964c8ace089
Alvesonik <lexabob.av@gmail.com>
2026-04-29 17:34:04 +0300

    create CRUD todo app

--- todo-app-backend/main.py
+++ todo-app-backend/main.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel
@@ -22,15 +22,40 @@ class TaskSchema(BaseModel):
 class TaskCreateSchema(BaseModel):
     title: str
 
+class TaskUpdateSchema(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
+
 tasks: list[TaskSchema] = []
 
 @app.get("/tasks")
 def read_tasks() -> list[TaskSchema]:
     return tasks
 
-@app.post("/tasks")
+@app.post("/tasks", status_code=status.HTTP_201_CREATED)
 def create_task(payload: TaskCreateSchema) -> TaskSchema:
     new_task = TaskSchema(id=str(uuid4()), title=payload.title, completed=False)
 
     tasks.append(new_task)
     return new_task
+
+@app.patch("/tasks/{task_id}")
+def update_task(payload: TaskUpdateSchema, task_id: str) -> TaskSchema:
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title is not None:
+                task.title = payload.title
+            if payload.completed is not None:
+                task.completed = payload.completed
+            return task
+
+    raise ValueError("Task not found")
+
+@app.delete("/tasks/{task_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_task(task_id: str) -> None:
+    for i, task in enumerate(tasks):
+        if task.id == task_id:
+            del tasks[i]
+            return
+
+    raise ValueError("Task not found")
